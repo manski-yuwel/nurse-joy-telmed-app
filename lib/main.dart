3a2719dd87c28bfd9effb1c5acf0ee89a02008f7
Midnight <luisgabrielle1026@gmail.com>
2025-02-14 00:38:38 +0800

    added firebase integration for android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'features/chat/ui/pages/chat_list_page.dart';
 import 'features/dashboard/ui/pages/dashboard_page.dart';
 import 'features/profile/ui/pages/profile_page.dart';
 
-void main() {
-  runApp(MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
