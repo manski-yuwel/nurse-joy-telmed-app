e8c3722b602d26138b9bbe941962168950184b67
Midnight <luisgabrielle1026@gmail.com>
2025-03-04 02:46:27 +0800

    added collection models for chat_room and messages

---
+++ lib/features/chat/data/models/models.dart
@@ -0,0 +1,14 @@
+// collection for the chat room
+final chat_room = {
+  'user1': '',
+  'user2': '',
+  'last_message': '',
+};
+
+// collection for the messages
+final messages = {
+  'senderID': '',
+  'recipientID': '',
+  'message_body': '',
+  'timestamp': DateTime.now(),
+};
