29269a9e397dedd83439a05b18e8c685931df5c7
Midnight <luisgabrielle1026@gmail.com>
2025-03-05 23:34:34 +0800

    added func for profile changing

--- lib/features/profile/data/profile_page_db.dart
+++ lib/features/profile/data/profile_page_db.dart
@@ -1,17 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+// initialize firestore and firebase auth
 final db = FirebaseFirestore.instance;
-
+final auth = FirebaseAuth.instance;
 Future<void> UpdateProfile(
     String userID,
+    String profilePicURL,
+    String email,
     String firstName,
     String lastName,
     String civilStatus,
     int age,
     DateTime birthdate,
     String address,
-    String phoneNumber) {
-  // update the user's profile
+    String phoneNumber) async {
+  // use auth to update the user's profile with the ones built-in the user type with firebase auth
+  await auth.currentUser!.updatePhotoURL(profilePicURL);
+  await auth.currentUser!.verifyBeforeUpdateEmail(email);
+  await auth.currentUser!.updateDisplayName('$firstName $lastName');
+
+  // update the user's profile through firestore
   return db.collection('users').doc(userID).update({
     'first_name': firstName,
     'last_name': lastName,
