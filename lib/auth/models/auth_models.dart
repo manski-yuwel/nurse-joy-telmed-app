7a682ebfbcdd0773cee2a8fe7494666b3decddd8
Midnight <luisgabrielle1026@gmail.com>
2025-05-26 20:58:23 +0800

    add: prototype doctor data model

--- lib/auth/models/auth_models.dart
+++ lib/auth/models/auth_models.dart
@@ -4,6 +4,9 @@ final user = {
   'profile_pic': '',
   'first_name': '',
   'last_name': '',
+  'full_name': '',
+  'full_name_lowercase': '',
+  'gender': '',
   'civil_status': '',
   'age': 0,
   'birthdate': null,
@@ -11,6 +14,8 @@ final user = {
   'phone_number': '',
   'role': 'user',
   'status_online': false,
+  'is_setup': false,
+  'search_index': [],
 };
 
 // data model for the user's health information
@@ -21,4 +26,19 @@ final healthInformation = {
   'allergies': [],
   'medications': [],
   'other_information': '',
+};
+
+// data model for doctor information
+final doctorData = {
+  'specialization': '',
+  'working_history': [],
+  'license_number': '',
+  'years_of_experience': 0,
+  'education': [],
+  'hospital_affiliation': [],
+  'rating': 0,
+  'num_of_ratings': 0,
+  'availability_schedule': [],
+  'consultation_fee': [],
+  'is_verified': false,
 };
